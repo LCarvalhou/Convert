23d0e56202ceaac897ba3628010e64cfb3356915
Leonardo Carvalho <leocarvalhoo97@gmail.com>
2026-05-08 16:35:13 -0400

    adicionando o calculo de conversão

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Cotacao de moedas do dia.
-const USD = 4.90;
+const USD = 4.91;
 const EUR = 5.77;
 const GBP = 6.67;
 
@@ -8,12 +8,13 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
-
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // manipulando o input amount para receber somente números
 amount.addEventListener("input",() => {        
     const hasCharectersRegex = /\D+/g;  
-})
+});
 
 //capturando o evento de submit= (ENVIAR) do formulario.
  form.onsubmit = (event) => {
@@ -38,19 +39,36 @@ amount.addEventListener("input",() => {
 // Função para conversao de moedas.
 function convertCurrency(amount, price, symbol) {
   try { 
+    // Exibindo a cotação da moeda selecionada.
+    description.innerText = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+    
+    // Calcula o valor total.
+    let total = amount * price;
+    
+    //Exibir o resultado total.
+    result.textContent = `${total} Reais`;
+
+
     //Aplica a classe que exibe o footer, com o resultado da conversão.        
- footer.classList.add("show-result");
+    footer.classList.add("show-result");
+
 
  } catch (error) {
    //Remove a classe que exibe o footer, removendo ele da tela.
+   
    footer.classList.remove("show-result");
-
+   
    console.log(error);
    alert("Ocorreu um erro ao converter a moeda. Por favor, tente novamente.");
 
  }
+//Formata a moeda para real brasileiro.
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
 
-
-
+}
 
 }
