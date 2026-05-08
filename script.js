6d5a0cdb043cc7f08197e465c1262da88a611a37
Leonardo Carvalho <leocarvalhoo97@gmail.com>
2026-05-08 15:34:54 -0400

    Input amount para receber apenas numeros

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(" teste OK");
+const amount=document.getElementById("amount");
+
+
+// MANIPULANDO O INPUT AMOUNT PARA RECEBER SOMENTE NÚMEROS
+amount.addEventListener("input",() => {        
+    const hasCharectersRegex = /\D+/g;  // Expressão regular para encontrar caracteres não numéricos
+    amount.value =amount.value.replace(hasCharectersRegex,"");
+
+
+
+})
